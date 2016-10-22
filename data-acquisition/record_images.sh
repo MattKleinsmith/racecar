cc12bf02c35efc7552af306b99d772236ba3774d
Matthew Kleinsmith <mwksmith@gmail.com>
2016-10-22 00:27:59 -0700

    Delete old folders

--- data-acquisition/record_images.sh
+++
@@ -1,7 +0,0 @@
-datetime=$(date +"%Y-%m-%d--%H-%M-%S")
-exthdd="/media/ubuntu/WDTB/"
-echo $datetime > /tmp/camera_datetime.txt
-echo $exthdd > /tmp/external_hard_drive.txt
-pathprefix=${exthdd}${datetime}
-date +%s%3N > ${pathprefix}_cam_clock.txt
-gst-launch-1.0 nvcamerasrc num-buffers=600 sensor-id=0 ! 'video/x-raw(memory:NVMM),width=320, height=160, framerate=30/1, format=NV12' ! nvvidconv flip-method=2 ! omxh264enc ! qtmux ! filesink location=${pathprefix}.mp4 -ev
