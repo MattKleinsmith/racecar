c5fba3cf52e03325a974292b6e58e3a42c752427
Matt Kleinsmith <mwksmith@gmail.com>
2016-11-02 17:27:49 -0700

    Adjust test_record_commands.sh w.r.t. Android changes
    Android changes: 1. Removed time from messages; 2. Added zero-padding

--- jetson/data-acquisition/tests/test_record_commands.sh
+++ jetson/data-acquisition/tests/test_record_commands.sh
@@ -1,30 +1,24 @@
-port=/dev/rfcomm0
+bluetooth=/dev/rfcomm0 # Connected to Android
+usb=/dev/ttyACM0 # Connected to Arduino
+cmd_log=commands.csv
+
 if [ "$#" -eq 1 ]; then 
 	channel=$1
 else
 	channel=$(sdptool browse --l2cap $phone | sed "1,/Jetson Remote Control/d" | \
 		grep -o 'Channel: [0-9]*' | head -1 | grep -o '[0-9]*')
 fi
-sudo rfcomm release $port
-sudo rfcomm bind $port $phone $channel
-date +%s%3N > jckl0.txt
+sudo rfcomm release $bluetooth
+sudo rfcomm bind $bluetooth $phone $channel
 
-#echo "aclk,throttle,steering" > commands.csv
-#cat $port >> commands.csv &
-
-#tail -f /dev/ttyACM0 &
-#while true; do
-#	tail -f commands.csv | cut -d, -f1- | tee -a arduino.csv /dev/ttyACM0
-#done
-
-file=z.csv
-cat /dev/null > $file
-cat /dev/null > $file.errors
+cat /dev/null > $cmd_log
+cat /dev/null > $cmd_log.errors
+#echo "aclk,throttle,steering" > $cmd_log
 while read line; do
   if [ ${#line} == 7 ]; then
-    echo $line >> $file
+    echo $line >> $usb
+    echo `date +%s%3N`,$line >> $cmd_log
   else
-    echo $line >> $file.errors
+    echo `date +%s%3N`,$line >> $cmd_log.errors
   fi
-done < $port
-
+done < $bluetooth
