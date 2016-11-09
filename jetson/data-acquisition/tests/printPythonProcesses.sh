d27188fa9ccf6d9bad3d9a7eea5e14a377cb16c1
Matt Kleinsmith <mwksmith@gmail.com>
2016-11-09 03:08:00 -0800

    Remove serial explorations

--- jetson/data-acquisition/tests/printPythonProcesses.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps -ef | grep ipython
--- jetson/data-acquisition/tests/ratioMsCmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-h=$(head -1 commands.csv.log | cut -d , -f 1)
-t=$(tail -1 commands.csv.log | cut -d , -f 1)
-ms=$(bc <<< "scale = 10; $t - $h")
-cmd=$(cat commands.csv.log | wc -l)
-echo
-echo "Average number of ms per command:"
-echo $(bc <<< "scale = 10; ($ms / $cmd)")
-echo
