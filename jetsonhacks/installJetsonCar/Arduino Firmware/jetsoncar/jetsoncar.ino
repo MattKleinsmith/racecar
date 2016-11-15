d2d8a5f5ed6f871520e749fb558fa7fcf4465ae9
Matt Kleinsmith <mwksmith@gmail.com>
2016-11-15 02:21:12 -0800

    Prepare Arduino to receive commands from the Android app

--- jetsonhacks/installJetsonCar/Arduino Firmware/jetsoncar/jetsoncar.ino
+++ jetsonhacks/installJetsonCar/Arduino Firmware/jetsoncar/jetsoncar.ino
@@ -20,19 +20,32 @@
 #include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
 
-ros::NodeHandle  nodeHandle;
+ros::NodeHandle nodeHandle;
 // These are general bounds for the steering servo and the
 // TRAXXAS Electronic Speed Controller (ESC)
-const int minSteering = 30 ;
-const int maxSteering = 150 ;
-const int minThrottle = 0 ;
-const int maxThrottle = 150 ;
+const int minSteering = 30;
+const int maxSteering = 150;
+const int minThrottle = 0;
+const int maxThrottle = 150;
+
+const int BAUDRATE = 115200;
+const int MAX_INPUT = 0;
+const int MAX_OUTPUT = 100;
+const int NEUTRAL_THROTTLE = 91;
+const int NEUTRAL_STEERING = 90;
+const int LED = 13;
+const int SETUP_DELAY = 1000;
+const int SPIN_DELAY = 1;
+const int STEERING_PIN = 9;
+const int THROTTLE_PIN = 11;
+const char *DEBUGGING_TOPIC = "/chatter";
+const char *COMMANDS_TOPIC = "/commands";
 
 Servo steeringServo;
-Servo electronicSpeedController ;  // The ESC on the TRAXXAS works like a Servo
+Servo electronicSpeedController; // The ESC on the TRAXXAS works like a Servo
 
 std_msgs::Int32 str_msg;
-ros::Publisher chatter("chatter", &str_msg); 
+ros::Publisher chatter(DEBUGGING_TOPIC, &str_msg); 
 
 // Arduino 'map' funtion for floating point
 double fmap (double toMap, double in_min, double in_max, double out_min, double out_max) {
@@ -42,64 +55,61 @@ double fmap (double toMap, double in_min, double in_max, double out_min, double 
 void driveCallback ( const geometry_msgs::Twist&  twistMsg )
 {
   
-  int steeringAngle = fmap(twistMsg.angular.z, 0.0, 1.0, minSteering, maxSteering) ;
+  int steeringAngle = fmap(twistMsg.angular.z, MAX_INPUT, MAX_OUTPUT, minSteering, maxSteering);
   // The following could be useful for debugging
-  // str_msg.data= steeringAngle ;
-  // chatter.publish(&str_msg);
+  int title = -1;
+  str_msg.data = title;
+  chatter.publish(&str_msg);
+  str_msg.data = steeringAngle;
+  chatter.publish(&str_msg);
   // Check to make sure steeringAngle is within car range
   if (steeringAngle < minSteering) { 
     steeringAngle = minSteering;
   }
   if (steeringAngle > maxSteering) {
-    steeringAngle = maxSteering ;
+    steeringAngle = maxSteering;
   }
-  steeringServo.write(steeringAngle) ;
+  steeringServo.write(steeringAngle);
   
-  // ESC forward is between 0.5 and 1.0
-  int escCommand ;
-  if (twistMsg.linear.x >= 0.5) {
-    escCommand = (int)fmap(twistMsg.linear.x, 0.5, 1.0, 90.0, maxThrottle) ;
-  } else {
-    escCommand = (int)fmap(twistMsg.linear.x, 0.0, 1.0, 0.0, 180.0) ;
-  }
+  int escCommand = fmap(twistMsg.linear.x, MAX_INPUT, MAX_OUTPUT, minThrottle, maxThrottle);
   // Check to make sure throttle command is within bounds
-  if (escCommand < minThrottle) { 
-    escCommand = minThrottle;
-  }
-  if (escCommand > maxThrottle) {
-    escCommand = maxThrottle ;
+  if (escCommand < minThrottle || escCommand > maxThrottle) { 
+    escCommand = NEUTRAL_THROTTLE;
   }
   // The following could be useful for debugging
-  // str_msg.data= escCommand ;
-  // chatter.publish(&str_msg);
+  int title2 = -2;
+  str_msg.data = title2;
+  chatter.publish(&str_msg);
+  str_msg.data = escCommand;
+  chatter.publish(&str_msg);
   
-  electronicSpeedController.write(escCommand) ;
-  digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+  electronicSpeedController.write(escCommand);
+  digitalWrite(LED, HIGH-digitalRead(LED));  //toggle led  
  
 }
 
-ros::Subscriber<geometry_msgs::Twist> driveSubscriber("/jetsoncar_teleop_joystick/cmd_vel", &driveCallback) ;
+ros::Subscriber<geometry_msgs::Twist> driveSubscriber(COMMANDS_TOPIC, &driveCallback);
 
 void setup(){
-  pinMode(13, OUTPUT);
-  Serial.begin(115200) ;
+  pinMode(LED, OUTPUT);
+  Serial.begin(BAUDRATE);
   nodeHandle.initNode();
   // This can be useful for debugging purposes
   nodeHandle.advertise(chatter);
   // Subscribe to the steering and throttle messages
-  nodeHandle.subscribe(driveSubscriber) ;
+  nodeHandle.subscribe(driveSubscriber);
   // Attach the servos to actual pins
-  steeringServo.attach(9); // Steering servo is attached to pin 9
-  electronicSpeedController.attach(11); // ESC is on pin 11
+  steeringServo.attach(STEERING_PIN);
+  electronicSpeedController.attach(THROTTLE_PIN);
   // Initialize Steering and ESC setting
   // Steering centered is 90, throttle at neutral is 90
-  steeringServo.write(90) ;
-  electronicSpeedController.write(90) ;
-  delay(1000) ;
+  steeringServo.write(NEUTRAL_STEERING);
+  electronicSpeedController.write(NEUTRAL_THROTTLE);
+  delay(SETUP_DELAY);
   
 }
 
 void loop(){
   nodeHandle.spinOnce();
-  delay(1);
+  delay(SPIN_DELAY);
 }
